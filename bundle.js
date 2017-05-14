0c5b5dc3431edb33d7a21ec8c6950702d94168fa
belousova-natallia <belousova_natallia@tut.by>
2017-05-14 12:35:39 +0300

    updates

--- bundle.js
+++ bundle.js
@@ -421,6 +421,51 @@ function renderPage() {
             __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__search__["a" /* search */])();
         }
     });
+
+    document.addEventListener('touchstart', handleTouchStart, false);
+    document.addEventListener('touchmove', handleTouchMove, false);
+
+    var xDown = null;
+    var yDown = null;
+
+    function handleTouchStart(evt) {
+        xDown = evt.touches[0].clientX;
+        yDown = evt.touches[0].clientY;
+    };
+
+    function handleTouchMove(evt) {
+        if (!xDown || !yDown) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = xDown - xUp;
+        var yDiff = yDown - yUp;
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {
+            /*most significant*/
+            if (xDiff > 0) {
+                /* left swipe */
+            } else {
+                    /* right swipe */
+                }
+        } else {
+            if (yDiff > 0) {
+                /* up swipe */
+                document.body.style.transform = 'translateY(-' + yDiff + 'px)';
+                document.body.style['transition-duration'] = '1s';
+            } else {
+                /* down swipe */
+                document.body.style.transform = 'translateY(' + yDiff + 'px)';
+                document.body.style['transition-duration'] = '1s';
+            }
+        }
+        /* reset values */
+        xDown = null;
+        yDown = null;
+    };
 }
 renderPage();
 
